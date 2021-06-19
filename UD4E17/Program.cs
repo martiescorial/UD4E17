f6f59f990844a709c6a2374f9e4481fb367c95c5
Martina <martinaescorial9@gmail.com>
2021-06-19 15:36:44 +0200

    Add project files.

---
+++ UD4E17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UD4E17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String texto = "Introduce el número de ventas";
+
+            int numVentas = Convert.ToInt32(texto);
+
+            int sumaVentas = 0;
+            for (int i = 0; i < numVentas; i++)
+            {
+                texto = ("Introduce el número de ventas" + (i + 1));
+                int venta = Convert.ToInt32(texto);
+
+                sumaVentas = sumaVentas + venta;
+            }
+
+            Console.WriteLine(sumaVentas);
+        }
+    }
+}
